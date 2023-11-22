da9d9d76182ca0f85d7befb4acab5c095947d2fd
unknown <a09694269@gmail.com>
2023-11-23 00:49:06 +0800

    livestream

---
+++ frontend/src/context/Themecontext.jsx
@@ -0,0 +1,3 @@
+import React ,{createContext, useContext} from "react";
+
+export const Themecontext = React.createContext(null);
---
+++ server/server.js
@@ -0,0 +1,5 @@
+import { Express } from "express";
+import mysql from "mysql"
+
+const app = express()
+app.use(express.json());
